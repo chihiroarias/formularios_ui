88b65afa875c81f2eaf18b28e997dd51e41e8e38
Bruno Arias <ariasssb@gmail.com>
2024-06-17 21:23:01 -0300

    update

--- src/Componentes/organismos/Campo/Campo.jsx
+++ src/Componentes/organismos/Campo/Campo.jsx
@@ -1,10 +1,7 @@
 //import {TipoCampo} from "../../moleculares/TipoCampo/TipoCampo.jsx"
-import { useDispatch } from "react-redux";
 import CampoFijo from "../../moleculares/CampoFijo/CampoFijo.jsx";
 import TipoCampo from "../../moleculares/TipoCampo/TipoCampo.jsx";
 function Campo() {
-  const dispatch = useDispatch();
-
   return (
     <div className="campo">
       <TipoCampo />
