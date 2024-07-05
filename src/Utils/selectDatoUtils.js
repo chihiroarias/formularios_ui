10d5efed7131aca4ad5f2d9fbe34d8dbfded212e
Bruno Arias <ariasssb@gmail.com>
2024-07-05 19:09:57 -0300

    comentairos

--- src/Utils/selectDatoUtils.js
+++ src/Utils/selectDatoUtils.js
@@ -11,10 +11,27 @@ export const selectDato = [
   { name: "Texto", value: "text" },
   { name: "Sección", value: "section" },
   //{ name: "Select", value: "select" },
-  
+
   { name: "Select", value: "select" },
-  { name: "Select anidado", value: "3"},
+  { name: "Select anidado", value: "3" },
   { name: "Selects existentes", value: "4" },
   { name: "Select precargado", value: "2" },
-  { name: "URL", value: "url" }
+  { name: "URL", value: "url" },
 ];
+/*
+id|nombreTipo            |
+--+----------------------+
+ 1|Área de texto         |
+ 2|Checkbox              |
+ 3|Fecha                 |
+ 4|Email                 |
+ 5|Número                |
+ 6|Radio                 |
+ 7|Teléfono              |
+ 8|Texto                 |
+ 9|Sección               |
+10|URL                   |
+11|Crear mi propio Select|
+12|Select                |
+13|Archivo               |
+*/
