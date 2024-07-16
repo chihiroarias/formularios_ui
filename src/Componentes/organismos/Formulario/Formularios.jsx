7265c35c2872c483a00d8565bccced582f19bbae
Bruno Arias <97006713+chihiroarias@users.noreply.github.com>
2024-07-16 15:53:27 -0300

    listado de formularios en funcionamiento

--- src/Componentes/organismos/Formulario/Formularios.jsx
+++ src/Componentes/organismos/Formulario/Formularios.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { accessAPI } from "../../../Utils/utils";
+import { useParams } from "react-router-dom";
 import MenuNavegacion from "../menuNavegacion/menuNavegacion.js";
 import Loader from "../../../elementos/loader/Loader";
 import "./formularios.css";
@@ -12,6 +13,8 @@ export default function Formularios() {
   const [formularios, setFormularios] = useState([]);
   const [formulario, setFormulario] = useState(null);
 
+  const { formularioId } = useParams();
+
   // Trae todos los formularios
   useEffect(() => {
     accessAPI(
@@ -19,19 +22,28 @@ export default function Formularios() {
       "admin/form/formularios/listadoformulario",
       null,
       (response) => {
-        setLoader(false);
         console.log(response);
         setFormularios(response);
+        setLoader(false);
       },
       (response) => {
         console.log(response);
       }
     );
-  }, []);
+    setFormularioSeleccionado(formularioId);
+  }, [formularioId]);
+
+  //Setea el loader dependiendo de si llegaron los datos
+  useEffect(() => {
+    if (formularios) {
+      setLoader(false);
+    }
+  }, [formularios]);
 
   // Cuando se selecciona un formulario en específico se cargan sus datos
   useEffect(() => {
     if (formularioSeleccionado) {
+      setFormulario();
       setLoaderFormulario(true);
       accessAPI(
         "GET",
@@ -45,75 +57,165 @@ export default function Formularios() {
         (response) => {
           console.log(response);
           setLoaderFormulario(false);
+          setFormularioSeleccionado();
         }
       );
     }
   }, [formularioSeleccionado]);
 
   return (
-    <div>
+    <div className="seccion laboratorio ">
       <MenuNavegacion submenuSeleccionado="formularios" />
-      <div>
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <div className="contenido ">
         {loader && <Loader>Cargando formulario</Loader>}
-        <div className="tarjetasLaboratoriosContainer flexContainer">
-          <div className="tarjeta listadoLaboratorios">
-            <h1>Listado de formularios</h1>
-            {formularios && formularios.length > 0 ? (
-              formularios.map((form) => (
-                <div
-                  className={
-                    formularioSeleccionado === form.id
-                      ? "laboratorio seleccionado"
-                      : "laboratorio"
-                  }
-                  key={form.id}
-                >
-                  <div onClick={() => setFormularioSeleccionado(form.id)}>
-                    {form.codigo}
+        {!loader && formularios && (
+          <>
+            <div className="tarjetasLaboratoriosContainer flexContainer">
+              <div className="tarjeta listadoLaboratorios">
+                <h1>Listado de formularios</h1>
+                {formularios.map((form) => {
+                  return (
+                    <div
+                      className={
+                        formularioSeleccionado === form.formid
+                          ? "laboratorio seleccionado"
+                          : "laboratorio"
+                      }
+                      key={form.formid}
+                    >
+                      <div
+                        onClick={() => {
+                          setFormularioSeleccionado(form.formid);
+                        }}
+                      >
+                        {form.formulario.codigo +
+                          " - " +
+                          form.formulario.titulo}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              {!formulario && loaderFormulario && (
+                <Loader>Cargando detalles del formulario</Loader>
+              )}
+              {formulario && (
+                <div>
+                  <div className="tarjeta laboratorioSeleccionado">
+                    <h1>Formulario: {formulario.formulario.titulo}</h1>
+                    <p>Descripción: {formulario.formulario.descripcion}</p>
+                    <p>Código: {formulario.formulario.codigo}</p>
+                    {formulario.campos && formulario.campos.length > 0 ? (
+                      formulario.campos.map((campo) => (
+                        <MuestraCampoForm
+                          key={campo.id}
+                          formversionid={campo.formversionid}
+                          labelForm={campo.labelForm}
+                          htmlFor={campo.htmlFor}
+                          placeholder={campo.placeholder}
+                          agrupacionRadio={campo.agrupacionRadio}
+                          regex={campo.regex}
+                          info={campo.info}
+                          indexadoForm={campo.indexadoForm}
+                          endpoint={campo.endpoint}
+                          condicional={campo.condicional}
+                          required={campo.required}
+                          selectPrecargadoId={campo.selectPrecargadoId}
+                          id={campo.id}
+                          type={campo.type}
+                        />
+                      ))
+                    ) : (
+                      <p>No hay campos disponibles para este formulario.</p>
+                    )}
                   </div>
                 </div>
-              ))
-            ) : (
-              <p>No hay formularios disponibles.</p>
-            )}
-          </div>
-          {!formulario && loaderFormulario && (
-            <Loader>Cargando detalles del formulario</Loader>
-          )}
-          {formulario && (
-            <div>
-              <div className="tarjeta laboratorioSeleccionado">
-                <h1>Formulario: {formulario.codigo}</h1>
-                <p>Descripción: {formulario.descripcion}</p>
-                <p>Código: {formulario.codigo}</p>
-                {formulario.campos && formulario.campos.length > 0 ? (
-                  formulario.campos.map((campo) => (
-                    <MuestraCampoForm
-                      key={campo.id}
-                      formversionid={campo.formversionid}
-                      labelForm={campo.labelForm}
-                      htmlFor={campo.htmlFor}
-                      placeholder={campo.placeholder}
-                      agrupacionRadio={campo.agrupacionRadio}
-                      regex={campo.regex}
-                      info={campo.info}
-                      indexadoForm={campo.indexadoForm}
-                      endpoint={campo.endpoint}
-                      condicional={campo.condicional}
-                      required={campo.required}
-                      selectPrecargadoId={campo.selectPrecargadoId}
-                      id={campo.id}
-                      type={campo.type}
-                    />
-                  ))
-                ) : (
-                  <p>No hay campos disponibles para este formulario.</p>
-                )}
-              </div>
+              )}
             </div>
-          )}
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
 }
+
+/**
+ 
+  return (
+    <div>
+      <MenuNavegacion submenuSeleccionado="formularios" />
+      <div>
+        {loader && <Loader>Cargando formulario</Loader>}
+        {!loader && formularios && (
+          <>
+            <div className="tarjetasLaboratoriosContainer flexContainer">
+              <div className="tarjeta listadoLaboratorios">
+                <h1>Listado de formularios</h1>
+                {formularios.map((form) => {
+                  return (
+                    <div
+                      className={
+                        formularioSeleccionado === form.value
+                          ? "  seleccionado"
+                          : "laboratorio"
+                      }
+                      key={form.value}
+                    >
+                      <div
+                        onClick={() => {
+                          setFormularioSeleccionado(form.value);
+                        }}
+                      >
+                        {form.codigo}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              {!formulario && loaderFormulario && (
+                <Loader>Cargando detalles del formulario</Loader>
+              )}
+              {formulario && (
+                <div>
+                  <div className="tarjeta laboratorioSeleccionado">
+                    <h1>Formulario: {formulario.codigo}</h1>
+                    <p>Descripción: {formulario.descripcion}</p>
+                    <p>Código: {formulario.codigo}</p>
+                    {formulario.campos && formulario.campos.length > 0 ? (
+                      formulario.campos.map((campo) => (
+                        <MuestraCampoForm
+                          key={campo.id}
+                          formversionid={campo.formversionid}
+                          labelForm={campo.labelForm}
+                          htmlFor={campo.htmlFor}
+                          placeholder={campo.placeholder}
+                          agrupacionRadio={campo.agrupacionRadio}
+                          regex={campo.regex}
+                          info={campo.info}
+                          indexadoForm={campo.indexadoForm}
+                          endpoint={campo.endpoint}
+                          condicional={campo.condicional}
+                          required={campo.required}
+                          selectPrecargadoId={campo.selectPrecargadoId}
+                          id={campo.id}
+                          type={campo.type}
+                        />
+                      ))
+                    ) : (
+                      <p>No hay campos disponibles para este formulario.</p>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+ */
