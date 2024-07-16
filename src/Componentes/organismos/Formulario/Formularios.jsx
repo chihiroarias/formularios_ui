bfb13b3490b9f08841b93ccfb9d732435179853a
Bruno Arias <97006713+chihiroarias@users.noreply.github.com>
2024-07-16 13:02:09 -0300

    formularios list

--- src/Componentes/organismos/Formulario/Formularios.jsx
+++ src/Componentes/organismos/Formulario/Formularios.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { accessAPI } from "../../../Utils/utils";
-import MenuNavegacion from "./menuNavegacion/menuNavegacion.js";
+import MenuNavegacion from "../menuNavegacion/menuNavegacion.js";
 import Loader from "../../../elementos/loader/Loader";
 import "./formularios.css";
 import MuestraCampoForm from "../../moleculares/MuestraCampoForm/MuestraCampoForm";
@@ -8,19 +8,20 @@ import MuestraCampoForm from "../../moleculares/MuestraCampoForm/MuestraCampoFor
 export default function Formularios() {
   const [loader, setLoader] = useState(true);
   const [loaderFormulario, setLoaderFormulario] = useState(false);
-  const [formularioSeleccionado, setFormularioSeleccionado] = useState(0);
-  const [formularios, setFormularios] = useState();
-  const [formulario, setFormulario] = useState();
+  const [formularioSeleccionado, setFormularioSeleccionado] = useState();
+  const [formularios, setFormularios] = useState([]);
+  const [formulario, setFormulario] = useState(null);
 
-  //trae todos los formularios
+  // Trae todos los formularios
   useEffect(() => {
     accessAPI(
       "GET",
       "admin/form/formularios/listadoformulario",
+      null,
       (response) => {
+        setLoader(false);
         console.log(response);
         setFormularios(response);
-        setLoader(false);
       },
       (response) => {
         console.log(response);
@@ -28,22 +29,25 @@ export default function Formularios() {
     );
   }, []);
 
-  //Cuando se selecciona un formulario en especifico se cargan sus datos
+  // Cuando se selecciona un formulario en específico se cargan sus datos
   useEffect(() => {
-    setLoaderFormulario(true);
-    accessAPI(
-      "GET",
-      `admin/form/formversion/${formularioSeleccionado}`,
-      (response) => {
-        console.log(response);
-        setFormulario(response);
-        setLoaderFormulario(false);
-      },
-      (response) => {
-        console.log(response);
-        setLoaderFormulario(false);
-      }
-    );
+    if (formularioSeleccionado) {
+      setLoaderFormulario(true);
+      accessAPI(
+        "GET",
+        `admin/form/formversion/${formularioSeleccionado}`,
+        null,
+        (response) => {
+          console.log(response);
+          setFormulario(response);
+          setLoaderFormulario(false);
+        },
+        (response) => {
+          console.log(response);
+          setLoaderFormulario(false);
+        }
+      );
+    }
   }, [formularioSeleccionado]);
 
   return (
@@ -53,8 +57,9 @@ export default function Formularios() {
         {loader && <Loader>Cargando formulario</Loader>}
         <div className="tarjetasLaboratoriosContainer flexContainer">
           <div className="tarjeta listadoLaboratorios">
-            {formularios.map((form) => {
-              return (
+            <h1>Listado de formularios</h1>
+            {formularios && formularios.length > 0 ? (
+              formularios.map((form) => (
                 <div
                   className={
                     formularioSeleccionado === form.id
@@ -67,8 +72,10 @@ export default function Formularios() {
                     {form.codigo}
                   </div>
                 </div>
-              );
-            })}
+              ))
+            ) : (
+              <p>No hay formularios disponibles.</p>
+            )}
           </div>
           {!formulario && loaderFormulario && (
             <Loader>Cargando detalles del formulario</Loader>
@@ -79,25 +86,29 @@ export default function Formularios() {
                 <h1>Formulario: {formulario.codigo}</h1>
                 <p>Descripción: {formulario.descripcion}</p>
                 <p>Código: {formulario.codigo}</p>
-                {formulario.campos.map((campo) => (
-                  <MuestraCampoForm
-                    key={campo.id}
-                    formversionid={campo.formversionid}
-                    labelForm={campo.labelForm}
-                    htmlFor={campo.htmlFor}
-                    placeholder={campo.placeholder}
-                    agrupacionRadio={campo.agrupacionRadio}
-                    regex={campo.regex}
-                    info={campo.info}
-                    indexadoForm={campo.indexadoForm}
-                    endpoint={campo.endpoint}
-                    condicional={campo.condicional}
-                    required={campo.required}
-                    selectPrecargadoId={campo.selectPrecargadoId}
-                    id={campo.id}
-                    type={campo.type}
-                  />
-                ))}
+                {formulario.campos && formulario.campos.length > 0 ? (
+                  formulario.campos.map((campo) => (
+                    <MuestraCampoForm
+                      key={campo.id}
+                      formversionid={campo.formversionid}
+                      labelForm={campo.labelForm}
+                      htmlFor={campo.htmlFor}
+                      placeholder={campo.placeholder}
+                      agrupacionRadio={campo.agrupacionRadio}
+                      regex={campo.regex}
+                      info={campo.info}
+                      indexadoForm={campo.indexadoForm}
+                      endpoint={campo.endpoint}
+                      condicional={campo.condicional}
+                      required={campo.required}
+                      selectPrecargadoId={campo.selectPrecargadoId}
+                      id={campo.id}
+                      type={campo.type}
+                    />
+                  ))
+                ) : (
+                  <p>No hay campos disponibles para este formulario.</p>
+                )}
               </div>
             </div>
           )}
@@ -106,66 +117,3 @@ export default function Formularios() {
     </div>
   );
 }
-
-/**
- * 
- * 
- * 
- * 
- * 
-<MenuNavegacion submenuSeleccionado="formularios" />
-      <div>
-        {loader && <Loader>Cargando formulario</Loader>}
-        <div className="tarjetasLaboratoriosContainer flexContainer">
-          <div className="tarjeta listadoLaboratorios">
-            {formularios.map((form) => {
-              return (
-                <div
-                  className={
-                    formularioSeleccionado === form.id
-                      ? "laboratorio seleccionado"
-                      : "laboratorio"
-                  }
-                  key={form.id}
-                >
-                  <div onClick={() => setFormularioSeleccionado(form.id)}>
-                    {form.codigo}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          {!formulario && loaderFormulario && (
-            <Loader>Cargando detalles del formulario</Loader>
-          )}
-          {formulario && (
-            <div>
-              <div className="tarjeta laboratorioSeleccionado">
-                <h1>Formulario: {formulario.codigo}</h1>
-                <p>Descripción: {formulario.descripcion}</p>
-                <p>Código: {formulario.codigo}</p>
-                {formulario.campos.map((campo) => (
-                  <MuestraCampoForm
-                    key={campo.id}
-                    formversionid={campo.formversionid}
-                    labelForm={campo.labelForm}
-                    htmlFor={campo.htmlFor}
-                    placeholder={campo.placeholder}
-                    agrupacionRadio={campo.agrupacionRadio}
-                    regex={campo.regex}
-                    info={campo.info}
-                    indexadoForm={campo.indexadoForm}
-                    endpoint={campo.endpoint}
-                    condicional={campo.condicional}
-                    required={campo.required}
-                    selectPrecargadoId={campo.selectPrecargadoId}
-                    id={campo.id}
-                    type={campo.type}
-                  />
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
- */
